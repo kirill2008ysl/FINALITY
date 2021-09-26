f8fd668e664bef5ae1478560105184e9e5ca54d1
kirillGOMEL2K08 <56223009+kirill2008ysl@users.noreply.github.com>
2021-09-26 14:19:09 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+﻿// console.log('Hi!');
+function changeVideo(url){
+    document.getElementById('video-slider').src = url;
+}
+
+//вкладка смена информации при потере активности
+
+let timer;
+let t = 'ItGid.info - красивая вкладка';
+
+function changeTitle(icon, text) {
+    document.querySelector('head title').innerHTML = text;
+    document.querySelector('link[rel="icon"]').setAttribute('href', icon);
+}
+
+window.onblur = () => {
+    timer = setTimeout(() => {
+        changeTitle('./heart.ico', '?? Вернитесь😪🤥😑');
+    }, 3000)
+}
+
+window.onfocus = () => {
+    clearTimeout(timer);
+    changeTitle('./favicon.ico', t);
+}
